4ac6e7797433f873b9f1fbc97013fba8e6b54d59
Brenden Cai <caibrenden@gmail.com>
2018-09-13 10:53:19 +1000

    Added Tesseract wrapper as singleton and utility method to consume pdf files

---
+++ Rosetta/OCR/Tesseract.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tesseract;
+
+namespace OCR
+{
+    public class Tesseract : IDisposable
+    {
+        private static TesseractEngine engine;
+
+        public static TesseractEngine getInstance()
+        {
+            if (engine != null)
+            {
+                return engine;
+            }
+            else
+            {
+                engine = new TesseractEngine(@"./tessdata", "eng", EngineMode.Default);
+                return engine;
+            }
+        }
+
+        public void Dispose()
+        {
+            if (engine != null)
+            {
+                engine.Dispose();
+            }
+        }
+    }
+
+    public static class Utiltiy
+    {
+        public static String Consume(String path)
+        {
+            string path_extension = Path.GetExtension(path);
+            if (path_extension != ".pdf")
+            {
+                throw new ArgumentException("Path does not lead to a PDF file type", path);
+            }
+            using (PDFiumEngine pdfEngine = new PDFiumEngine(path))
+            using (StreamWriter sw = new StreamWriter(String.Format("output_{0}.txt", Path.GetFileNameWithoutExtension(path), false)))
+            {
+                for (int i = 0; i < pdfEngine.PageCount; i++)
+                {
+                    Image img = pdfEngine.GetImageFromPageNumber(i, 4000, 0);
+                    using (TesseractEngine engine = Tesseract.getInstance())
+                    {
+                        Bitmap bit = new Bitmap(img);
+                        using (Page p = engine.Process(bit, PageSegMode.AutoOsd))
+                        {
+                            string text = p.GetHOCRText(0);
+                            sw.WriteLine(text);
+                            sw.Flush();
+                        }
+                    }
+                }
+                sw.Close();
+            }
+
+            return "";
+        }
+    }
+}

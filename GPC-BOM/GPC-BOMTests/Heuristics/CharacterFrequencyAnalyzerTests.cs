b4d3160f71336bc5a69271d15dc7edc7657aa218
Brenden Cai <caibrenden@gmail.com>
2018-10-02 01:34:01 +1000

    Added basic zero/empty/null set tests for FrequencyAnalysis

--- GPC-BOM/GPC-BOMTests/Heuristics/CharacterFrequencyAnalyzerTests.cs
+++ GPC-BOM/GPC-BOMTests/Heuristics/CharacterFrequencyAnalyzerTests.cs
@@ -12,9 +12,30 @@ namespace GPC_BOM.Heuristics.Tests
     public class CharacterFrequencyAnalyzerTests
     {
         [TestMethod()]
-        public void AnalyzeTest()
+        public void AnalyzeTest_EmptyTestZero()
         {
-            throw new NotImplementedException();
+            Dictionary<int, int> result = CharacterFrequencyAnalyzer.Analyze(new List<string> { "" });
+
+            Assert.IsNotNull(result);
+            Assert.IsFalse(result.Any(e => e.Value > 0));
+        }
+
+        [TestMethod()]
+        public void AnalyzeTest_EmptySetNull()
+        {
+            Dictionary<int, int> result = GPC_BOM.Heuristics.CharacterFrequencyAnalyzer.Analyze(null);
+            Assert.IsNotNull(result);
+            Assert.IsFalse(result.Any(e => e.Value > 0));
+        }
+
+        [TestMethod()]
+        public void AnalyzeTest_SingleSet()
+        {
+            Dictionary<int, int> result = CharacterFrequencyAnalyzer.Analyze(new List<string> { "ABC" });
+
+            Assert.AreEqual(1, result['A']);
+            Assert.AreEqual(1, result['B']);
+            Assert.AreEqual(1, result['C']);
         }
     }
 }

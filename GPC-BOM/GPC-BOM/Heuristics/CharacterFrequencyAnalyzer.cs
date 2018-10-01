d255836c27f6f18f110f1a60d91137b500ebe173
Brenden Cai <caibrenden@gmail.com>
2018-10-02 01:35:01 +1000

    Implemented basic algorithm to loop through strings and count frequency of characters.

--- GPC-BOM/GPC-BOM/Heuristics/CharacterFrequencyAnalyzer.cs
+++ GPC-BOM/GPC-BOM/Heuristics/CharacterFrequencyAnalyzer.cs
@@ -10,7 +10,24 @@ namespace GPC_BOM.Heuristics
     {
         public static Dictionary<int, int> Analyze(List<string> input)
         {
-            throw new NotImplementedException();
+            Dictionary<int, int> charFrequencyObservations = new Dictionary<int, int>();
+            for (int i = 0; i < 128; i++)
+            {
+                charFrequencyObservations.Add(i, 0);
+            }
+
+            foreach (string s in input)
+            {
+                foreach (char c in s)
+                {
+                    if (charFrequencyObservations.TryGetValue(c, out int foundValue))
+                    {
+                        charFrequencyObservations[c]++;
+                    }
+                }
+            }
+
+            return charFrequencyObservations;
         }
     }
 

07ed8e51f92e5cece6b21b4e4bf73ba0435256e0
Brenden Cai <caibrenden@gmail.com>
2018-10-02 00:59:04 +1000

    Base template for Heuristics

---
+++ GPC-BOM/GPC-BOM/Heuristics/CharacterFrequencyAnalyzer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPC_BOM.Heuristics
+{
+    public static class CharacterFrequencyAnalyzer
+    {
+        public static Dictionary<int, int> Analyze(List<string> input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
---
+++ GPC-BOM/GPC-BOMTests/Heuristics/CharacterFrequencyAnalyzerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GPC_BOM.Heuristics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPC_BOM.Heuristics.Tests
+{
+    [TestClass()]
+    public class CharacterFrequencyAnalyzerTests
+    {
+        [TestMethod()]
+        public void AnalyzeTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

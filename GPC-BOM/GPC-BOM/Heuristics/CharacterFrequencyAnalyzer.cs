15dc5f014fb941f4d2b0bde4e3d0c8de9c36eeef
Brenden Cai <caibrenden@gmail.com>
2018-10-11 06:08:26 +1100

    Added a few overloads of aggregation and analysis methods

--- GPC-BOM/GPC-BOM/Heuristics/CharacterFrequencyAnalyzer.cs
+++ GPC-BOM/GPC-BOM/Heuristics/CharacterFrequencyAnalyzer.cs
@@ -34,6 +34,30 @@ namespace GPC_BOM.Heuristics
 
             return charFrequencyObservations;
         }
+        public static Dictionary<int, int> Analyze(IEnumerable<string> input)
+        {
+            Dictionary<int, int> charFrequencyObservations = new Dictionary<int, int>();
+            for (int i = 0; i < 128; i++)
+            {
+                charFrequencyObservations.Add(i, 0);
+            }
+
+            if (input != null)
+            {
+                foreach (string s in input)
+                {
+                    foreach (char c in s)
+                    {
+                        if (charFrequencyObservations.TryGetValue(c, out int foundValue))
+                        {
+                            charFrequencyObservations[c]++;
+                        }
+                    }
+                }
+            }
+
+            return charFrequencyObservations;
+        }
 
         public static Dictionary<int, decimal> CalculateHueristicValue()
         {
@@ -138,6 +162,121 @@ namespace GPC_BOM.Heuristics
             return categoricalFrequency;
         }
 
+
+        public static Dictionary<int, double> CalculateRelativeFrequency(IEnumerable<string> input)
+        {
+            Dictionary<int, double> frequency = new Dictionary<int, double>();
+            Dictionary<int, int> f = Analyze(input);
+
+            var totalCharCount = f.Sum(kp => kp.Value);
+            foreach(KeyValuePair<int, int> kvp in f)
+            {
+                double relativeFrequency = (double)kvp.Value / totalCharCount;
+                frequency.Add(kvp.Key, relativeFrequency);
+            }
+
+            return frequency;
+        }
+
+        public static Dictionary<Category, double> CalculateRelativeAggregateFrequency(IEnumerable<string> input)
+        {
+            Dictionary<Category, double> categoricalFrequency = new Dictionary<Category, double>();
+            Dictionary<int, int> f = Analyze(input);
+            var totalCharCount = f.Sum(kp => kp.Value);
+
+            foreach (KeyValuePair<int, int> keyPair in f)
+            {
+
+                if (keyPair.Key == 32)
+                {
+                    categoricalFrequency[Category.Space] += keyPair.Value;
+                }
+                if (keyPair.Key == 44)
+                {
+                    categoricalFrequency[Category.Comma] += keyPair.Value;
+                }
+                if (keyPair.Key >= 32 && keyPair.Key <= 47)
+                {
+                    categoricalFrequency[Category.Punctuation]++;
+                }
+
+                // Numbers
+                if (keyPair.Key >= 48 && keyPair.Key <= 57)
+                {
+                    categoricalFrequency[Category.Numbers] += keyPair.Value;
+                }
+
+                // Uppercase Characters && Lowercase Characters
+                if (keyPair.Key >= 65)
+                {
+                    if (keyPair.Key <= 90)
+                    {
+                        categoricalFrequency[Category.Uppercase] += keyPair.Value;
+                    }
+                    else if (keyPair.Key <= 122)
+                    {
+                        categoricalFrequency[Category.Lowercase] += keyPair.Value;
+                    }
+                }
+            }
+            foreach(KeyValuePair<Category, double> keyPair in categoricalFrequency)
+            {
+                categoricalFrequency[keyPair.Key] = keyPair.Value / totalCharCount;
+                
+            }
+
+            return categoricalFrequency;
+        }
+
+        public static Dictionary<Category, double> CalculateRelativeAggregateFrequency(IEnumerable<double> input)
+        {
+            Dictionary<Category, double> categoricalFrequency = new Dictionary<Category, double>();
+            Dictionary<int, int> f = new Dictionary<int, int>();
+            var totalCharCount = f.Sum(kp => kp.Value);
+
+            for(int i = 0; i < input.Count(); i++)
+            {
+
+                if (i == 32)
+                {
+                    categoricalFrequency[Category.Space] += input.ElementAt(i);
+                }
+                if (i == 44)
+                {
+                    categoricalFrequency[Category.Comma] += input.ElementAt(i);
+                }
+                if (i >= 32 && i <= 47)
+                {
+                    categoricalFrequency[Category.Punctuation]++;
+                }
+
+                // Numbers
+                if (i >= 48 && i <= 57)
+                {
+                    categoricalFrequency[Category.Numbers] += input.ElementAt(i);
+                }
+
+                // Uppercase Characters && Lowercase Characters
+                if (i >= 65)
+                {
+                    if (i <= 90)
+                    {
+                        categoricalFrequency[Category.Uppercase] += input.ElementAt(i);
+                    }
+                    else if (i <= 122)
+                    {
+                        categoricalFrequency[Category.Lowercase] += input.ElementAt(i);
+                    }
+                }
+            }
+            foreach (KeyValuePair<Category, double> keyPair in categoricalFrequency)
+            {
+                categoricalFrequency[keyPair.Key] = keyPair.Value / totalCharCount;
+
+            }
+
+            return categoricalFrequency;
+        }
     }
 
 }

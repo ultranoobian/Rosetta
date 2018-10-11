2bee8cb0caf7c1f94df30c9ea8298330a96f5282
Brenden Cai <caibrenden@gmail.com>
2018-10-11 11:58:33 +1100

    Template test files

---
+++ GPC-BOM/GPC-BOMTests/Heuristics/ClassifierTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GPC_BOM.Heuristics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPC_BOM.Heuristics.Tests
+{
+    [TestClass()]
+    public class ClassifierTests
+    {
+        [TestMethod()]
+        public void GetInstanceAnalyze_EmptySetZero()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void AddFrequencyValueAnalyze_EmptySetZero()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void AddFrequencyValueAnalyze_EmptySetZero1()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void ClassifyAnalyze_EmptySetZero()
+        {
+            Classifier clf = Classifier.GetInstance();
+            PrivateObject privateObject = new PrivateObject(clf);
+            var a = (List<List<double>>)privateObject.GetField("qtyClassifierValues");
+            int i = a.Count;
+        }
+    }
+}

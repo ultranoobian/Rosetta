11399eeeac08855918d4ec479b5df3f0aeacf2e9
Brenden Cai <caibrenden@gmail.com>
2018-10-18 04:09:26 +1100

    Added new tests for kNN for all of the different column types

--- GPC-BOM/GPC-BOMTests/Heuristics/ClassifierTests.cs
+++ GPC-BOM/GPC-BOMTests/Heuristics/ClassifierTests.cs
@@ -162,5 +162,139 @@ namespace GPC_BOM.Heuristics.Tests
 
             Assert.AreEqual(testColumn, result);
         }
+
+        [TestMethod()]
+        public void ClassificationTest_Classify_kNN_3_Manufacturer()
+        {
+            const Classifier.ColumnType testColumn = Classifier.ColumnType.Manufacturer;
+            Classifier clf = Classifier.GetInstance();
+            Assert.IsNotNull(clf);
+
+            LoadFrequencyValues(clf);
+
+            var result = clf.Classify_KNN(new List<string>()
+            {
+                "TI", "Murata", "AVX", "Keystone", "Microchip" ,"Crystek Corporation"
+            }, 3);
+
+            Assert.AreEqual(testColumn, result);
+        }
+        [TestMethod()]
+        public void ClassificationTest_Classify_kNN_5_Manufacturer()
+        {
+            const Classifier.ColumnType testColumn = Classifier.ColumnType.Manufacturer;
+            Classifier clf = Classifier.GetInstance();
+            Assert.IsNotNull(clf);
+
+            LoadFrequencyValues(clf);
+
+            var result = clf.Classify_KNN(new List<string>()
+            {
+                "TI", "Murata", "AVX", "Keystone", "Microchip" ,"Crystek Corporation"
+            }, 5);
+
+            Assert.AreEqual(testColumn, result);
+        }
+
+        [TestMethod()]
+        public void ClassificationTest_Classify_kNN_3_Designator()
+        {
+            const Classifier.ColumnType testColumn = Classifier.ColumnType.Designator;
+            Classifier clf = Classifier.GetInstance();
+            Assert.IsNotNull(clf);
+
+            LoadFrequencyValues(clf);
+
+            var result = clf.Classify_KNN(new List<string>()
+            {
+                "C2,C3,C14,C15,C22,C23,C28,C29,C48,C49,C61,C62,C74,C75,C86,C87,C94-C173,R22,R24,R36,R39,R65,R87,R90,R94",
+                "T1,T2,T3,T4", "L19_QSFP0, L19_QSFP1, L22_QSFP0, L22_QSFP1, L23_QSFP0, L23_QSFP1"
+            }, 3);
+
+            Assert.AreEqual(testColumn, result);
+        }
+        [TestMethod()]
+        public void ClassificationTest_Classify_kNN_5_Designator()
+        {
+            const Classifier.ColumnType testColumn = Classifier.ColumnType.Designator;
+            Classifier clf = Classifier.GetInstance();
+            Assert.IsNotNull(clf);
+
+            LoadFrequencyValues(clf);
+
+            var result = clf.Classify_KNN(new List<string>()
+            {
+                "C2,C3,C14,C15,C22,C23,C28,C29,C48,C49,C61,C62,C74,C75,C86,C87,C94-C173,R22,R24,R36,R39,R65,R87,R90,R94",
+                "T1,T2,T3,T4", "L19_QSFP0, L19_QSFP1, L22_QSFP0, L22_QSFP1, L23_QSFP0, L23_QSFP1"
+            }, 5);
+
+            Assert.AreEqual(testColumn, result);
+        }
+
+        [TestMethod()]
+        public void ClassificationTest_Classify_kNN_3_Description()
+        {
+            const Classifier.ColumnType testColumn = Classifier.ColumnType.Description;
+            Classifier clf = Classifier.GetInstance();
+            Assert.IsNotNull(clf);
+
+            LoadFrequencyValues(clf);
+
+            var result = clf.Classify_KNN(new List<string>()
+            {
+                "Ferrite Bead, 220R @ 100M", "Ferrite Bead, 220R @ 100M", "Header, 3 way, 1.25mm pitch", "Header, 14 way, 2mm pitch, SMT", "High Speed USB UART/FIFO"
+            }, 3);
+
+            Assert.AreEqual(testColumn, result);
+        }
+        [TestMethod()]
+        public void ClassificationTest_Classify_kNN_5_Description()
+        {
+            const Classifier.ColumnType testColumn = Classifier.ColumnType.Description;
+            Classifier clf = Classifier.GetInstance();
+            Assert.IsNotNull(clf);
+
+            LoadFrequencyValues(clf);
+
+            var result = clf.Classify_KNN(new List<string>()
+            {
+                "Ferrite Bead, 220R @ 100M", "Ferrite Bead, 220R @ 100M", "Header, 3 way, 1.25mm pitch", "Header, 14 way, 2mm pitch, SMT", "High Speed USB UART/FIFO"
+            }, 5);
+
+            Assert.AreEqual(testColumn, result);
+        }
+
+        [TestMethod()]
+        public void ClassificationTest_Classify_kNN_3_Quantity()
+        {
+            const Classifier.ColumnType testColumn = Classifier.ColumnType.Quantity;
+            Classifier clf = Classifier.GetInstance();
+            Assert.IsNotNull(clf);
+
+            LoadFrequencyValues(clf);
+
+            var result = clf.Classify_KNN(new List<string>()
+            {
+                "1","7","3","4","2","1","4","4","2","3","2","1"
+            }, 3);
+
+            Assert.AreEqual(testColumn, result);
+        }
+        [TestMethod()]
+        public void ClassificationTest_Classify_kNN_5_Quantity()
+        {
+            const Classifier.ColumnType testColumn = Classifier.ColumnType.Quantity;
+            Classifier clf = Classifier.GetInstance();
+            Assert.IsNotNull(clf);
+
+            LoadFrequencyValues(clf);
+
+            var result = clf.Classify_KNN(new List<string>()
+            {
+                "1","7","3","4","2","1","4","4","2","3","2","1"
+            }, 5);
+
+            Assert.AreEqual(testColumn, result);
+        }
     }
 }

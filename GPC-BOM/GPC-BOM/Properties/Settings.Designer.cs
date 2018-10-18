fa86f1516c7d37146afe23186f0fcdc8c7f82fac
Brenton <brenton>
2018-10-18 17:46:26 +1100

    Reduce default web scraper timeout

--- GPC-BOM/GPC-BOM/Properties/Settings.Designer.cs
+++ GPC-BOM/GPC-BOM/Properties/Settings.Designer.cs
@@ -253,7 +253,7 @@ namespace GPC_BOM.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("15")]
+        [global::System.Configuration.DefaultSettingValueAttribute("10")]
         public int webTimeout {
             get {
                 return ((int)(this["webTimeout"]));

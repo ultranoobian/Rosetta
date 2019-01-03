be0e3f4ea250b1de1f35c46c8aa66be3213b7daf
Brenden Cai <caibrenden@gmail.com>
2019-01-04 10:27:37 +1100

    Filled in OAuth variables for DigiKey and provided code attribution

--- GPC-BOM/GPC-BOM/DigiKeyOAuth.cs
+++ GPC-BOM/GPC-BOM/DigiKeyOAuth.cs
@@ -6,12 +6,16 @@ using System.Threading.Tasks;
 
 namespace GPC_BOM
 {
+    /// <summary>
+    ///  Adapted from: OAuth for Apps: Samples for Windows
+    ///  By: googlesamples
+    ///  At:  https://github.com/googlesamples/oauth-apps-for-windows
+    /// </summary>
     class DigiKeyOAuth
     {
-        const string clientID = "";
-        const string clientSecret = "";
-        const string authEndPoint = "";
-        const string tokenEndPoint = "";
-        const string userInfoEndpoint = "";
+        const string clientID = "a78ec1fe-e228-4f68-ae31-10aaf4e3104a";
+        const string clientSecret = ""; // Not going to put secret here
+        const string authEndPoint = "https://sso.digikey.com/as/authorization.oauth2";
+        const string tokenEndPoint = "https://sso.digikey.com/as/token.oauth2";
     }
 }

e4d9bb77be98da4987e298c542f076666d8d43ea
Brenden Cai <caibrenden@gmail.com>
2019-01-04 09:58:46 +1100

    Template for Digikey OAuth 2.0

---
+++ GPC-BOM/GPC-BOM/DigiKeyOAuth.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPC_BOM
+{
+    class DigiKeyOAuth
+    {
+        const string clientID = "";
+        const string clientSecret = "";
+        const string authEndPoint = "";
+        const string tokenEndPoint = "";
+        const string userInfoEndpoint = "";
+    }
+}

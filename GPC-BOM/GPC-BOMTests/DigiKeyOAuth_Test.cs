8241cf39a526021b03d5e443a11781fa1e7d9031
Brenden Cai <caibrenden@gmail.com>
2019-01-07 00:40:31 +1100

    Added Test DigiKey API entry point for debugging purposes

---
+++ GPC-BOM/GPC-BOMTests/DigiKeyOAuth_Test.cs
@@ -0,0 +1,18 @@
+﻿using GPC_BOM;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GPC_BOM.Tests
+{
+    [TestClass()]
+    public class DigiKeyOAuth_Test
+    {
+        [TestMethod()]
+        public void authProcess_test()
+        {
+            DigiKeyOAuth auth = new DigiKeyOAuth();
+            auth.authProcess();
+            //throw new NotImplementedException();
+        }
+    }
+}
